4f4f17fb2b3150cb96f5cc49228bfb0544c83471
Aiden H <aiden.hernandez@d11.org>
2024-10-31 18:24:10 +0000

    tuples!

--- tuple.py
+++ tuple.py
@@ -1,18 +1,24 @@
 print("Question 1:")
 #Q1: Create a tuple with single item 50 and print your tuple
-
+tuple0 = ("50",)
+print(tuple0)
 print()
 print("Question 2:")
 #Q2: Access the value 4 and print it from tuple1
 tuple1 = ("Purple", "Pink", 17, [1, 2, 3, 4, 5], (5, 15, 25))
-
+print(tuple1[3][3])
 
 print()
 print("Question 3:")
 #Q3: Create a new tuple with 6 foods called foods.
+foods = ("burger", "pizza", "apple", "Count Chocula", "Pasta", "food 6")
 # a. Print the tuple
+print(foods)
 # b. Print the length
+print(len(foods))
 # c. Print the first item in your tuple
+print(foods[0])
 # d. Print the last item in your tuple
+print(foods[5])
 
 
